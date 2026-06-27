e01ce04a2c697f108c747bc50c5052ff8e46cfca
maxbotchway <maxcollinsbotchway88@gmail.com>
2026-06-27 15:35:00 +0200

    Simplify CRUUZ landing page

--- app/ride/page.tsx
+++ app/ride/page.tsx
@@ -1,5 +1,3 @@
-import Navbar from "@/components/layout/Navbar";
-import Footer from "@/components/layout/Footer";
 import RideHero from "@/components/ride/RideHero";
 import HowItWorks from "@/components/ride/HowItWorks";
 import RidePageTypes from "@/components/ride/RidePageTypes";
@@ -9,13 +7,11 @@ import RideCTA from "@/components/ride/RideCTA";
 export default function RidePage() {
   return (
     <main className="min-h-screen bg-[#101936] text-white">
-      <Navbar />
       <RideHero />
       <HowItWorks />
       <RidePageTypes />
       <RideSafety />
       <RideCTA />
-      <Footer />
     </main>
   );
 }
